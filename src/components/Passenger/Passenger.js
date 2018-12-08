9c97ae41a455895e83282729d88f27fd707bc8a9
Daily120 <forzhacker@mail.ru>
2018-12-08 22:11:40 +0100

    delete passenger

--- src/components/Passenger/Passenger.js
+++ src/components/Passenger/Passenger.js
@@ -1,5 +1,31 @@
 import React from 'react';
 
+const deletePassengerFromUI = (passengerRow) => {
+    console.log(passengerRow);
+    passengerRow.parentNode.parentNode.parentNode.removeChild(passengerRow.parentNode.parentNode);
+}
+
+const displayError = () => {
+    alert('Error deleting passenger!')
+}
+
+const handleDeletePassenger = (event) => {
+    const passenger = event.target;
+    const passengerId = passenger.id.split(' ')[1];
+    
+    fetch('http://localhost:3001/passengers', {
+        method: 'DELETE',
+        headers: {
+            authorization: `Bearer ${localStorage.token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: passengerId
+        })
+    }).then(res => res.json())
+    .then(data => data === 'Success' ? deletePassengerFromUI(passenger) : displayError())
+}
+
 const Passenger = ({ id, title, firstName, surname, passportId }) => {
 
     return (
@@ -8,7 +34,7 @@ const Passenger = ({ id, title, firstName, surname, passportId }) => {
             <td>{firstName}</td>
             <td>{surname}</td>
             <td>{passportId}</td>
-            <td><button id={`p ${id}`} className="deleteButton">Delete</button></td>
+            <td><button onClick={handleDeletePassenger} id={`p ${id}`} className="deleteButton">Delete</button></td>
             <td className="lastCol"></td>
         </tr>
     )
